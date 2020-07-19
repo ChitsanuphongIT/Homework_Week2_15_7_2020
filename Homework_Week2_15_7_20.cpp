83268606f15600deb05d0efc8da5ebf93090de8f
ChitsanuphongIT <67995734+ChitsanuphongIT@users.noreply.github.com>
2020-07-19 15:32:14 +0700

    Update Homework_Week2_15_7_20.cpp

--- Homework_Week2_15_7_20.cpp
+++ Homework_Week2_15_7_20.cpp
@@ -8,21 +8,29 @@ void main(){
 	short salary, sale, percent, total;
 
 	cout << "****Homework 2************" << endl;
+	
 	cout << "Enter Name : ";
 	cin >> firstname >> lastname;
+	
 	cout << "Enter Salary : ";
 	cin >> salary;
+	
 	cout << "Enter Sale : ";
 	cin >> sale;
+	
 	cout << "Commission Percent : ";
 	cin >> percent;
 
 	total = (sale*percent/100)+salary;
 
 	cout << "- - - - - -output - - - - - - - - - -" << endl;
+	
 	cout << "Your name = " << firstname << " " << lastname << endl;
 	cout << "Total Revenue " << total << " Bath" << endl;
+	
 	cout << "- - - - - - - - - - - - - - - - - - - - - - - - - - - - -" << endl;
+	
 	// Chitsanuphong Chaihong 6306021621138 no.23
+	
 	system("pause");
 }
